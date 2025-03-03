1ed1be9089692451c4c364b1fc4aacf5f2922541
hisamat7 <muhammedhisam10@gmail.com>
2025-03-04 04:58:29 +0530

    created loginpage

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +9,160 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget listContainer({
+    required String text,
+    required String image
+  }) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+          color: Colors.grey[100], borderRadius: BorderRadius.circular(30)),
+      child: ListTile(
+        leading: Image.asset(image),
+        title: Text(text),
+        trailing: Icon(Icons.arrow_forward_ios_rounded),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        surfaceTintColor: Colors.amberAccent,
+        leading: Icon(
+          Icons.arrow_back_ios,
+          size: 20,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text("Register"),
+          )
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 150,
+                width: double.infinity,
+                color: Colors.amberAccent,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 26),
+                  child: Text(
+                    "Sign in",
+                    style:
+                        TextStyle(fontSize: 30.sp, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            // Main container
+            Positioned(
+              top:
+                  130, // Adjust this value to position the container below the "Sign in" text
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 246, 246),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 350,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.only(left: 16, right: 16),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 60.h,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey[100],
+                                filled: true,
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.h,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey[100],
+                                filled: true,
+                                hintText: "Email",
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            ListTile(
+                              trailing: Text(
+                                "Forget password ?",
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Login",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  minimumSize: Size(double.infinity, 60)),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          padding: EdgeInsets.only(left: 16, right: 16),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 30.h,
+                              ),
+                              listContainer(text: "Continue with google",image:'asset/image/google-icon 1.png' ),
+                              listContainer(text: "Continue with facebook",image:'asset/image/facebook-3 1.png' ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
